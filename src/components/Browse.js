7e869c6fd95a49aa29d09791444f3e709c9699ad
benyyyy <106058071+benyyyy@users.noreply.github.com>
2025-01-28 14:44:46 +0530

    tmdb data fetch

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import Header from './Header'
+import useNowPlaying from '../hooks/useNowPlaying'
+import MainContainer from './MainContainer'
+import SecondaryContainer from './SecondaryContainer'
+
 
 const Browse = () => {
+useNowPlaying()
   return (
    <>
     <Header/>
+    <MainContainer/>
+    <SecondaryContainer/>
+    {/* mainContainer
+    -videobackground
+    --video title
+    secondary mainContainer
+    -movieLiser*N
+    -CARDS*n */}
+    
+   
     <h2>browse</h2>
    </>
   )
--- src/utils/AppStrore.js
+++ src/utils/AppStrore.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import moviesReducer from './MoviesSlice'
 import userReducer from './UserSlice'
 const appStore=configureStore({
     reducer: {
-        user:userReducer
+        user:userReducer,
+        movies:moviesReducer,
     },
 });
 export default appStore;
